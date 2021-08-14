689bd4ce1808ddb5ef26c963c80ade38f8bc3831
Lawrence Gumbo <84715458+lawrence-sert@users.noreply.github.com>
2021-08-14 09:05:27 +0200

    auth redo

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 import { AuthGuard } from "src/app/guards/auth.guard";
 import { IntroGuard } from 'src/app/guards/intro.guard';
+import { AutoLoginGuard } from './guards/auto-login.guard';
 
 const routes: Routes = [
   {
@@ -12,7 +13,7 @@ const routes: Routes = [
   {
     path: 'sign-in',
     loadChildren: () => import('./auth/sign-in/sign-in.module').then( m => m.SignInPageModule),
-    canLoad: [IntroGuard] // Check if we should show the introduction or forward to inside
+    canLoad: [IntroGuard, AutoLoginGuard] // Check if we should show the introduction or forward to inside
   },
   {
     path: 'sign-up',
@@ -28,7 +29,8 @@ const routes: Routes = [
   },
   {
     path: 'dashboard',
-    loadChildren: () => import('./auth/dashboard/dashboard.module').then( m => m.DashboardPageModule) // Secure all child pages
+    loadChildren: () => import('./auth/dashboard/dashboard.module').then( m => m.DashboardPageModule),
+    canLoad: [AuthGuard] // Secure all child pages
   },
 
   //to remove
